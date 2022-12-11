6d48d5fa08d7bb6872ba72959b5c42d69bb84575
Jayvee Bustarde <jayvee.bustarde@gmail.com>
2022-12-12 00:01:28 +0100

    day10

---
+++ Day10/Program.cs
@@ -0,0 +1,182 @@
+﻿string[] input = File.ReadAllLines("C:\\Code\\AdventOfCode2022\\Day10\\input");
+string[] input_ = new string[]
+{
+"addx 15",
+"addx -11",
+"addx 6",
+"addx -3",
+"addx 5",
+"addx -1",
+"addx -8",
+"addx 13",
+"addx 4",
+"noop",
+"addx -1",
+"addx 5",
+"addx -1",
+"addx 5",
+"addx -1",
+"addx 5",
+"addx -1",
+"addx 5",
+"addx -1",
+"addx -35",
+"addx 1",
+"addx 24",
+"addx -19",
+"addx 1",
+"addx 16",
+"addx -11",
+"noop",
+"noop",
+"addx 21",
+"addx -15",
+"noop",
+"noop",
+"addx -3",
+"addx 9",
+"addx 1",
+"addx -3",
+"addx 8",
+"addx 1",
+"addx 5",
+"noop",
+"noop",
+"noop",
+"noop",
+"noop",
+"addx -36",
+"noop",
+"addx 1",
+"addx 7",
+"noop",
+"noop",
+"noop",
+"addx 2",
+"addx 6",
+"noop",
+"noop",
+"noop",
+"noop",
+"noop",
+"addx 1",
+"noop",
+"noop",
+"addx 7",
+"addx 1",
+"noop",
+"addx -13",
+"addx 13",
+"addx 7",
+"noop",
+"addx 1",
+"addx -33",
+"noop",
+"noop",
+"noop",
+"addx 2",
+"noop",
+"noop",
+"noop",
+"addx 8",
+"noop",
+"addx -1",
+"addx 2",
+"addx 1",
+"noop",
+"addx 17",
+"addx -9",
+"addx 1",
+"addx 1",
+"addx -3",
+"addx 11",
+"noop",
+"noop",
+"addx 1",
+"noop",
+"addx 1",
+"noop",
+"noop",
+"addx -13",
+"addx -19",
+"addx 1",
+"addx 3",
+"addx 26",
+"addx -30",
+"addx 12",
+"addx -1",
+"addx 3",
+"addx 1",
+"noop",
+"noop",
+"noop",
+"addx -9",   //
+"addx 18",
+"addx 1",
+"addx 2",
+"noop",
+"noop",
+"addx 9",
+"noop",
+"noop",
+"noop",
+"addx -1",
+"addx 2",
+"addx -37",
+"addx 1",
+"addx 3",
+"noop",
+"addx 15",
+"addx -21",
+"addx 22",
+"addx -6",
+"addx 1",
+"noop",
+"addx 2",
+"addx 1",
+"noop",
+"addx -10",
+"noop",
+"noop",
+"addx 20",
+"addx 1",
+"addx 2",
+"addx 2",
+"addx -6",
+"addx -11",
+"noop",
+"noop",
+"noop"
+};
+
+int[] signals = new int[] { 20, 60, 100, 140, 180, 220 };
+int x = 1;
+int cycle = 1;
+int totalStrength = 0;
+string crtRow = "";
+for (int i = 0; i < input.Length; i++)
+{
+    if (input[i] == "noop")
+        processCycle();
+    if (input[i][..4] == "addx")
+    {
+        processCycle();
+        processCycle();
+        int y = int.Parse(input[i][5..]);
+        x += y;
+    }
+}
+void processCycle()
+{
+    if (signals.Contains(cycle))
+        totalStrength += cycle * x;
+    crtRow += Math.Abs((cycle % 40) - (x + 1)) > 1 ? "." : "#";
+    if (cycle % 40 == 0)
+    {
+        Console.WriteLine((crtRow) + " " + crtRow.Length);
+        crtRow = "";
+    }
+    cycle++;
+}
+
+Console.WriteLine(totalStrength);
--- Day9/Program.cs
+++ Day9/Program.cs
@@ -1,65 +1,72 @@
 ﻿//string[] input = File.ReadAllLines("C:\\Code\\AdventOfCode2022\\Day9\\input");
-string[] input = new string[]
-{
-"R 4",
-"U 4",
-"L 3",
-"D 1",
-"R 4",
-"D 1",
-"L 5",
-"R 2"
-};
-
+string[] input = new string[] {
+"R 5",
+"U 8",
+"L 8",
+"D 3",
+"R 17",
+"D 10",
+"L 25",
+"U 20" };
 List<Tuple<int, int>> tPositions = new();
+List<Tuple<int, int>[]> part2Positions = new();
 var (hx, hy, tx, ty) = (0, 0, 0, 0);
 for (int i = 0; i < input.Length; i++)
 {
+    part2Positions.Add(new Tuple<int, int>[10]);
     int move = int.Parse(input[i][2..]);
-
+    tx = part2Positions.Last()[1]?.Item1 ?? 0;
+    ty = part2Positions.Last()[1]?.Item2 ?? 0;
     if (input[i][0] == 'D')
     {
         for (int j = hy; j >= hy - move; j--)
-        {
-            ComputeMove(tPositions, move, hx, hy, ref tx, ref ty);
-        }
+            ComputeMove(tPositions, 0, hx, j, ref tx, ref ty);
         hy -= move;
     }
     else if (input[i][0] == 'U')
     {
-        for (int j = ty; j <= ty + move; j++)
-        {
-            ComputeMove(tPositions, move, hx, hy, ref tx, ref ty);
-        }
+        for (int j = hy; j <= hy + move; j++)
+            ComputeMove(tPositions, 0, hx, j, ref tx, ref ty);
         hy += move;
     }
-    if (input[i][0] == 'L')
+    else if (input[i][0] == 'L')
     {
-        for (int j = tx; j >= tx - move; j--)
-        {
-            ComputeMove(tPositions, move, hx, hy, ref tx, ref ty);
-        }
-        tx -= move;
+        for (int j = hx; j >= hx - move; j--)
+            ComputeMove(tPositions, 0, j, hy, ref tx, ref ty);
+        hx -= move;
     }
     else if (input[i][0] == 'R')
     {
-        for (int j = tx; j <= tx + move; j++)
-        {
-            ComputeMove(tPositions, move, hx, hy, ref tx, ref ty);
-        }
-        tx += move;
+        for (int j = hx; j <= hx + move; j++)
+            ComputeMove(tPositions, 0, j, hy, ref tx, ref ty);
+        hx += move;
     }
 }
-Console.WriteLine(tPositions.Count);
+Console.WriteLine("Part1: " + tPositions.Count);
 
-void ComputeMove(List<Tuple<int, int>> tPositions, int move, int hx, int hy, ref int tx, ref int ty)
+void ComputeMove(List<Tuple<int, int>> tPositions, int index, int hx, int hy, ref int tx, ref int ty)
 {
-    if (Math.Abs(hx - tx) > 1 || Math.Abs(hy - ty) > 1)
+    if (Math.Abs(hx - tx) + Math.Abs(hy - ty) > 2)
     {
-        tx += Math.Abs(hx - tx) > 1 ? (hx > tx) ? 1 : -1 : 0;
-        ty += Math.Abs(hy - ty) > 1 ? (hy > ty) ? 1 : -1 : 0;
-        int tempTx = tx, tenpTy = ty;
+        tx += hx > tx ? 1 : -1;
+        ty += hy > ty ? 1 : -1;
+    }
+    else if (Math.Abs(hx - tx) > 1 || Math.Abs(hy - ty) > 1)
+    {
+        tx += Math.Abs(hx - tx) > 1 ? hx > tx ? 1 : -1 : 0;
+        ty += Math.Abs(hy - ty) > 1 ? hy > ty ? 1 : -1 : 0;
+    }
+    int tempTx = tx, tenpTy = ty;
+    part2Positions.Last()[index] = new(tx, ty);
+    if (index == 9)
+    {
         if (!tPositions.Any(x => x.Item1 == tempTx && x.Item2 == tenpTy))
             tPositions.Add(new(tx, ty));
+        return;
     }
+    int newtx = 0, newty = 0;
+    if(part2Positions.Count > 1)
+        (newtx, newty) = part2Positions.Skip(part2Positions.Count - 1).Take(1).FirstOrDefault()[index];
+
+    ComputeMove(tPositions, index + 1, tx, ty, ref newtx, ref newty);
 }
